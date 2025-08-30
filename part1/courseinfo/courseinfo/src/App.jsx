25d29beba1b8d68303354fa7166a0a46963d42a0
kevin-n-01 <knebe001@icloud.com>
2025-08-30 01:37:07 -0400

    Completed 1.5

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -7,12 +7,13 @@ const Header = ({course}) => {
   )
 }
 
-const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+const Content = ({parts}) => {
+  const [part1, part2, part3] = parts;
   return (
     <div>
-      <Part name={part1} numExercises={exercises1} />
-      <Part name={part2} numExercises={exercises2} />
-      <Part name={part3} numExercises={exercises3} />
+      <Part name={part1.name} numExercises={part1.exercises} />
+      <Part name={part2.name} numExercises={part2.exercises} />
+      <Part name={part3.name} numExercises={part3.exercises} />
     </div>
   )
 }
@@ -25,68 +26,38 @@ const Part = (coursePart) => {
   )
 }
 
-const Footer = ({exercises1, exercises2, exercises3}) => {
+const Footer = ({parts}) => {
+  const [part1, part2, part3] = parts;
   return (
     <div>
-      <p>Total number of exercises: {exercises1 + exercises2 + exercises3}</p>
+      <p>Total number of exercises: {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
   
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        parts={course.parts}
       />
       <Footer 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        parts={course.parts}
       />
     </div>
   )
 
 }
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-
-//   return (
-//     <div>
-//       <h1>{course}</h1>
-//       <p>
-//         {part1} {exercises1}
-//       </p>
-//       <p>
-//         {part2} {exercises2}
-//       </p>
-//       <p>
-//         {part3} {exercises3}
-//       </p>
-//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-//     </div>
-//   )
-// }
 
 export default App
