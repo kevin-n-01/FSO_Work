8519454350ad064e5023d207cb56c4b197d0e357
kevin-n-01 <knebe001@icloud.com>
2025-11-06 01:43:21 -0500

    Imported express and completed 3.1 - 3.2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001
+
+app.use(express.json());
+
+const now = new Date().toLocaleString();
+
+let contacts = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(contacts);
+})
+
+//API Request for General Info
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${contacts.length} people.</p>
+        <p>${now}`)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
