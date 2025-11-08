13d19302ec489bc7e5252f578a60afcafcbfa825
kevin-n-01 <knebe001@icloud.com>
2025-11-07 22:53:57 -0500

    completed 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 3001
 
 app.use(express.json());
 
-app.use(morgan('tiny'));
+morgan.token('summary', (request) => JSON.stringify(request.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :summary'));
 
 const now = new Date().toLocaleString();
 
