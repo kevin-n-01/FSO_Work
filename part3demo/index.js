c8c46a2dc861627a892d5cce543ce01fc6b6aa35
kevin-n-01 <knebe001@icloud.com>
2026-01-05 23:28:58 -0500

    removed cors from development as server is now handled through proxy

--- part3demo/index.js
+++ part3demo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors()); // Allow all origins during development
+//app.use(cors()); // Allow all origins during development
 
 app.use(express.json());
 
